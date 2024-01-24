1bbcdbc9bf010498d13848b72973b4fe5e26e824
hctaw-srp <scarsdaleny15@yahoo.co.jp>
2024-01-24 18:40:02 +0900

    ~fix baseurl

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:4321',
+    baseUrl: 'http://localhost:4321',
     supportFile: false,
   },
 })
